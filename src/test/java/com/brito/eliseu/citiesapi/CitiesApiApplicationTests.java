a6af854e9f64c97bed2e3125480bf5cd5765ec03
eliseusbrito <eliseusbrito@gmail.com>
2021-05-10 11:03:52 -0300

    Projeto conforme start.spring.io

---
+++ src/test/java/com/brito/eliseu/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brito.eliseu.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
