be67e8b1e38e93bf7ea40fc8740dbc02595efffe
eliseusbrito <eliseusbrito@gmail.com>
2021-05-11 10:24:09 -0300

    Alterado para pageable o endpoint de cidade

--- src/main/java/com/brito/eliseu/citiesapi/controller/CityController.java
+++ src/main/java/com/brito/eliseu/citiesapi/controller/CityController.java
@@ -2,11 +2,11 @@ package com.brito.eliseu.citiesapi.controller;
 
 import com.brito.eliseu.citiesapi.domain.City;
 import com.brito.eliseu.citiesapi.repository.CityRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("cities")
@@ -19,8 +19,8 @@ public class CityController {
     }
 
   @GetMapping
-  public List<City> cities() {
-      return repository.findAll();
+  public Page<City> cities(final Pageable page) {
+      return repository.findAll(page);
   }
 
 
