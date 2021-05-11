8390e8917328af4da414d7acc2748dacfc378ffa
eliseusbrito <eliseusbrito@gmail.com>
2021-05-11 11:42:46 -0300

    Adicionado objeto point para poder fazer calculos usando extensoes do postgree

--- src/main/java/com/brito/eliseu/citiesapi/domain/City.java
+++ src/main/java/com/brito/eliseu/citiesapi/domain/City.java
@@ -1,5 +1,10 @@
 package com.brito.eliseu.citiesapi.domain;
 
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.springframework.data.geo.Point;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +12,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "cidade")
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
 public class City {
 
     @Id
@@ -22,7 +30,21 @@ public class City {
     @Column(name = "lat_lon")
     private String geolocation;
 
+    @Type(type = "point")
+    @Column(name = "lat_lon", updatable = false, insertable = false)
+    private Point location;
+
     public City() {
+    }
+
+    public City(final Long id, final String name, final Integer uf, final Integer ibge,
+                final String geolocation, final Point location) {
+        this.id = id;
+        this.name = name;
+        this.uf = uf;
+        this.ibge = ibge;
+        this.geolocation = geolocation;
+        this.location = location;
     }
 
     public Long getId() {
@@ -43,6 +65,10 @@ public class City {
 
     public String getGeolocation() {
         return geolocation;
+    }
+
+    public Point getLocation() {
+        return location;
     }
 
 }
